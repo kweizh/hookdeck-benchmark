5f178ffe5d2afaa6f564adae6a528fb81909926f
Wei Zhang <kweizh@tabbyml.com>
2026-06-11 13:09:35 +0000

    fix: cleanup

--- jobs/2026-06-10__17-35-13/transformation_missing_fields_cl__TZSTiKr/artifacts/user/hookdeck-task/transform.js
+++
@@ -1,7 +0,0 @@
-addHandler('transform', (request, context) => {
-  if (!request.body || typeof request.body !== 'object') {
-    request.body = {};
-  }
-  request.body.user_id = request.body?.data?.user?.id ?? null;
-  return request;
-});
--- jobs/2026-06-11__05-54-28/retrieve_request_volume_metrics__pFYKbeu/artifacts/user/hookdeck-metrics/get_metrics.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -s -X GET "https://api.hookdeck.com/2025-07-01/metrics/requests" \
-  -H "Authorization: Bearer ${HOOKDECK_API_KEY}" \
-  -o metrics.json
